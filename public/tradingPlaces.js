d40bacc13574d552bb599812f41447e040a107fe
Alex <aikluger@gmail.com>
2019-03-14 10:11:07 -0400

    adding trading places in process

---
+++ public/tradingPlaces.js
@@ -0,0 +1,61 @@
+{
+    "type": "Feature",
+    "properties": {
+        "title": "The Duke & Duke Bank (Wells Fargo Building)",
+        "movie": "Trading Places",
+        "icon": "star",
+        "descriptionURL": "https://en.wikipedia.org/wiki/Wells_Fargo_Building_(Philadelphia)",
+        "description": "The center of business for the Dukes in the film, this actual bank was built in 1928 and is listed on the National Register of Historic Places",
+        "image":"http://4.bp.blogspot.com/-F853Ijg3TYw/Uejh3WflB9I/AAAAAAAAII0/1Pj38X1ILD0/s1600/DukeAndDuke2_135+S+Broad_TradingPlaces.jpg"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-75.0952, 39.5658]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "title": "Trading Places Statue",
+        "movie": "Trading Places",
+        "icon": "monument",
+        "descriptionURL": "https://totalrocky.com/filming-location-guide/the-philadelphia-museum-of-art.html",
+        "description": "The Philadelphia Museum of Art steps is one of the most famous movie locations of all time and a popular tourist attraction.",
+        "image": "https://totalrocky.com/wp-content/uploads/2010/12/guide_steps5.jpg"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-75.179285, 39.965169]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "title": "Ben Franklin Bridge",
+        "movie": "National Treasure",
+        "icon": "star",
+        "descriptionURL": "https://www.imdb.com/title/tt0368891/trivia?item=tr3028540",
+        "description": "When the characters are seen entering Philadelphia, they are crossing on the Benjamin Franklin Bridge, a nod to the Founding Fathers theme in the movie.",
+        "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/2012_Ben_Franklin_Bridge_and_Race_Street_Pier.jpg/300px-2012_Ben_Franklin_Bridge_and_Race_Street_Pier.jpg"
+
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-75.134414, 39.952935]
+    }
+},
+{
+    "type": "Feature",
+    "properties": {
+        "title": "Franklin Institute",
+        "movie": "National Treasure",
+        "icon": "museum",
+        "descriptionURL": "https://nationaltreasure.fandom.com/wiki/Franklin_Institute",
+        "description": "The Franklin Institute is a museum in Philadelphia dedicated to Benjamin Franklin where the second clue to the Templar Treasure is. Patrick Henry Gates donated the Silence Dogood letters to the Institute.",
+        "image": "https://assets.visitphilly.com/wp-content/uploads/2018/02/The-Franklin-Institute-Franklin-Statue-J-Fusco-1200VP.jpg"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-75.172637, 39.958205]
+    }
+},
