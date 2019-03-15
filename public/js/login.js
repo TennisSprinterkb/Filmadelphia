084f92f390a8f5dc5aac75a4f8813c93d5a9cf04
Suzanne Hunter <sue_huntr@yahoo.com>
2019-03-15 14:47:25 -0400

    login page video and form functional

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
+  $('#formScreen').hide();
   // Getting references to our form and inputs
   var loginForm = $("form.login");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
+// timer for showing form
+  var signupShow = function(){
+    $('#formScreen').show();
+  };
+  setTimeout(signupShow, 4000);
 
   // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function(event) {
+  // loginForm.on("submit", function(event) {
+    $("#logIn").on("click", function(event) {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
